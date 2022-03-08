f4c13c2907c627d79570838077c1ea966992ed52
myleftshoe <paulesammut@gmail.com>
2022-03-08 17:05:27 +1100

    .

---
+++ .vercel_build_output/static/_app/pages/index.svelte-3e6a81ec.js
@@ -0,0 +1,5 @@
+import{S as K,i as y,s as V,e as v,t as c,k as W,c as u,a as _,h,d as l,m as b,b as q,g as x,J as o,K as k}from"../chunks/vendor-bccf472e.js";function A(E){let e,d,n,a,m,s,p,f;return{c(){e=v("h1"),d=c("Welcome to SvelteKit"),n=W(),a=v("p"),m=c(`Visit
+    `),s=v("a"),p=c("kit.svelte.dev"),f=c(`
+    to read the documentation`),this.h()},l(t){e=u(t,"H1",{});var i=_(e);d=h(i,"Welcome to SvelteKit"),i.forEach(l),n=b(t),a=u(t,"P",{});var r=_(a);m=h(r,`Visit
+    `),s=u(r,"A",{href:!0});var S=_(s);p=h(S,"kit.svelte.dev"),S.forEach(l),f=h(r,`
+    to read the documentation`),r.forEach(l),this.h()},h(){q(s,"href","https://kit.svelte.dev")},m(t,i){x(t,e,i),o(e,d),x(t,n,i),x(t,a,i),o(a,m),o(a,s),o(s,p),o(a,f)},p:k,i:k,o:k,d(t){t&&l(e),t&&l(n),t&&l(a)}}}class H extends K{constructor(e){super();y(this,e,null,A,V,{})}}export{H as default};
