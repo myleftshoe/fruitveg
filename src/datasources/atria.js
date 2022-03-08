a35f9053acdaf7d82414a6b68423394e3cda1788
myleftshoe <paulesammut@gmail.com>
2022-03-08 19:18:24 +1100

    .

--- src/datasources/atria.js
+++ src/datasources/atria.js
@@ -20,11 +20,8 @@ async function get(path) {
 async function login() {
     console.log('login')
     const browser = await puppeteer.launch({
-        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security", "--no-sandbox"],
-        defaultViewport: chrome.defaultViewport,
-        executablePath: await chrome.executablePath,
+        args: chrome.args,
         headless: true,
-        ignoreHTTPSErrors: true,
     })
     const [ page ] = await browser.pages()
 
