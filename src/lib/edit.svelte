410677928ff6f9fed0230db774db83bddaee8883
myleftshoe <paulesammut@gmail.com>
2022-03-11 17:11:24 +1100

    .

---
+++ src/lib/edit.svelte
@@ -0,0 +1,44 @@
+<script>
+    import List, { Item, Text, PrimaryText, SecondaryText, Meta } from '@smui/list'
+    import Dialog, { Title, Content, Actions } from '@smui/dialog'
+    import Button, { Label } from '@smui/button'
+    import Textfield from '@smui/textfield'
+    import Switch from '@smui/switch'
+
+    export let item = { 
+        label5: '',
+        label4: ''
+    }
+    let checked = false
+    $: open = Boolean(item.id)
+    $: price = item.UnitPrice || 0
+    $: console.log(item)
+</script>
+<Dialog bind:open on:SMUIDialog:closed={() => (item = {})}>
+    <Title>{item.Description}</Title>
+    <Content>
+    <Textfield 
+        style="width: 100%;"
+        bind:value={item.label5}
+    />    
+    <Textfield
+        style="width: 100%;"
+        bind:value={item.label4}
+    />    
+    <Textfield 
+        style="width: 30%;"
+        prefix="$"
+        bind:value={price}
+    />
+    <Textfield 
+        style="width: 20%;"
+        bind:value={item.unit}
+    />
+    <!-- <Switch bind:checked icons={false} /> -->
+    </Content>
+    <Actions>
+        <Button on:click={() => (item = {})}>
+            <Label>Save</Label>
+        </Button>
+    </Actions>
+</Dialog>
--- src/routes/products/index.svelte
+++ src/routes/products/index.svelte
@@ -1,5 +1,10 @@
 <script context="module">
     import 'carbon-components-svelte/css/white.css'
+    import List, { Item, Text, PrimaryText, SecondaryText, Meta } from '@smui/list'
+    import Dialog, { Title, Content, Actions } from '@smui/dialog'
+    import Button, { Label } from '@smui/button'
+    import Textfield from '@smui/textfield'
+    import TopAppBar, { Row, Section } from '@smui/top-app-bar';
     import {
         DataTable,
         Toolbar,
@@ -12,7 +17,9 @@
 </script>
 
 <script>
+    import EditDialog from '$lib/edit.svelte'
     export let products = []
+    let selectedRow
     $: value = value?.toUpperCase?.() ?? ''
     $: headers = getheaders(products)
     $: rows = fuzzy(products, value)
@@ -21,34 +28,45 @@
         ItemCodeHeader.empty = true
         ItemCodeHeader.sort = false
     }
+    $: console.log(value)
 </script>
-
-<DataTable
-    zebra
-    sortable
-    {headers}
-    {rows}
->
+<TopAppBar>
     <Toolbar>
         <ToolbarContent>
             <ToolbarSearch bind:value expanded persistent/>
         </ToolbarContent>
     </Toolbar>
-    <svelte:fragment slot="cell-header" let:header>
-        {#if header.key === "id"}
-            PLUCODE
-        {:else}
-            {header.value}
-        {/if}
-    </svelte:fragment>
-    <svelte:fragment slot="cell" let:row let:cell>
-        {#if cell.key === "Description"}
-            <Link inline href={`products/${row.ItemCode}`}>
-                {cell.value}
-            </Link>
-        {:else}
-            <div hidden={cell.key === 'ItemCode'}>{cell.value}</div>
-        {/if}
-    </svelte:fragment>
-
-</DataTable>
+</TopAppBar>
+<main>
+<List threeLine>
+    {#each rows as row}
+        <Item on:SMUI:action={() => (selectedRow = row)}>
+            <Text>
+                <PrimaryText>{row.Description}</PrimaryText>
+                <SecondaryText>{row.label5 || ''}</SecondaryText>
+                <SecondaryText>{row.label4 || ''}</SecondaryText>
+            </Text>
+            <Meta>
+                <div>
+                    <h4>{row.UnitPrice}</h4>
+                    <pre>{row.unit || ''}</pre>
+                </div>
+            </Meta>
+        </Item>
+    {/each}
+</List>
+</main>
+<EditDialog item={selectedRow}/>
+<style>
+    main { 
+        position: absolute;
+        top: 40px ;
+        width: 100%
+    }
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        align-content: center;
+    }
+</style>
