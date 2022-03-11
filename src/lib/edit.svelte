15a2007a50e0b309e5e85fb6f1d460e3efd4790b
myleftshoe <paulesammut@gmail.com>
2022-03-11 18:07:14 +1100

    dialog style

--- src/lib/edit.svelte
+++ src/lib/edit.svelte
@@ -3,38 +3,40 @@
     import Dialog, { Title, Content, Actions } from '@smui/dialog'
     import Button, { Label } from '@smui/button'
     import Textfield from '@smui/textfield'
-    import Switch from '@smui/switch'
 
     export let item = { 
         label5: '',
-        label4: ''
+        label4: '',
+        UnitPrice: 0,
+        unit: '',
     }
     let checked = false
     $: open = Boolean(item.id)
-    $: price = item.UnitPrice || 0
     $: console.log(item)
 </script>
 <Dialog bind:open on:SMUIDialog:closed={() => (item = {})}>
     <Title>{item.Description}</Title>
     <Content>
-    <Textfield 
-        style="width: 100%;"
-        bind:value={item.label5}
-    />    
-    <Textfield
-        style="width: 100%;"
-        bind:value={item.label4}
-    />    
-    <Textfield 
-        style="width: 30%;"
-        prefix="$"
-        bind:value={price}
-    />
-    <Textfield 
-        style="width: 20%;"
-        bind:value={item.unit}
-    />
-    <!-- <Switch bind:checked icons={false} /> -->
+        <p></p>
+        <div>
+            <Textfield 
+                style="width: 100%;"
+                bind:value={item.label5}
+            />    
+            <Textfield
+                style="width: 100%;"
+                bind:value={item.label4}
+            />    
+        </div>
+        <Textfield 
+            style="width: 40%;"
+            prefix="$"
+            bind:value={item.UnitPrice}
+        />
+        <Textfield 
+            style="width: 30%;"
+            bind:value={item.unit}
+        />
     </Content>
     <Actions>
         <Button on:click={() => (item = {})}>
@@ -42,3 +44,12 @@
         </Button>
     </Actions>
 </Dialog>
+<style>
+div { 
+    border: 3px solid #000b;
+    border-radius: 18px;
+    padding: 18px;
+    padding-top: 0px;
+    margin-top: 24px;
+}
+</style>
