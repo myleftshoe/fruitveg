b4ed9846c60ce9cda9317423219e3377a96c89f4
myleftshoe <paulesammut@gmail.com>
2022-03-18 01:00:42 +1100

    .

--- src/routes/stocktake/edit.svelte
+++ src/routes/stocktake/edit.svelte
@@ -44,6 +44,11 @@
 
     let qty_ref
 
+    $: if (qty_ref) {
+        console.log('focus', qty_ref.focus)
+        qty_ref.focus()
+    }
+
     $: open = Boolean(selectedRow)
 </script>
 
@@ -52,8 +57,8 @@
         position: fixed;
         top:0;
         left:0;
-        height:50%;
-        width:100%;
+        height:50vh;
+        width:100vw;
         display: flex;
         flex-direction: column;
         align-items: center;
@@ -77,9 +82,37 @@
         justify-content: flex-center;
         width:100%;
     }
+    input {
+        border: none;
+        outline:none;
+        background-color: transparent;
+        color: white;
+    }
+    units {
+        transform: scale(.8) translateY(-8px);
+        display:flex;
+    }
+    hr {
+        width:100%; 
+        height: 1px;
+        color:#fff1; 
+    }
+    #qty {
+        font-size: 5ch;
+    }
+    #notes {
+        font-size: 2ch;
+        width: 100%; 
+        border:none;
+        background-color: #7772;
+        /* border-radius: 16px; */
+        padding: 16px;
+        color:#fffd;
+        font-family: sans; 
+    }
 </style>
 
-<Dialog fullscreen bind:open on:SMUIDialog:closed={handleClose} style="height: 50vh">
+<Dialog fullscreen bind:open on:SMUIDialog:closed={handleClose} style="height: 50vh; width: 100vw;">
 {#if selectedRow}
     <main 
         _style="background-color:var({parseInt(selectedRow.qty) >= 0 || selectedRow.notes ? '--mdc-theme-primary' : ''});"
@@ -93,7 +126,7 @@
                 close
             </IconButton>
         </div> -->
-        <Content style="display:flex; flex-direction: column; align-items: center; gap: 3%;">
+        <Content style="display:flex; flex-direction: column; align-items: center; justify-content: space-between; gap: 3%;">
             <Title style={parseInt(selectedRow.qty) >= 0 || selectedRow.notes ? 'color: var(--mdc-theme-primary)' : ''}>{selectedRow.Description}</Title>
             <hidden>
                 <input
@@ -107,43 +140,51 @@
                     name="Description"
                     bind:value={selectedRow.Description} />
             </hidden>
-            <Textfield
-                textarea
-                label="Notes"
-                input$id="notes"
-                input$name="notes"
-                style="width: 100%;"
-                bind:value={selectedRow.notes}
-            />
             <horzflex>
-                <IconButton touch class="material-icons" on:click={prev} style="">arrow_back_ios</IconButton>
+                <IconButton touch class="material-icons" on:click={prev} tabindex="-1">arrow_back_ios</IconButton>
                 <vertflex>
-                    <Textfield
+                    <input
                         bind:this={qty_ref}
                         label="Quantity"
-                        input$type="tel"
-                        style="width: 50%;"
-                        input$step=1
-                        input$min=0
-                        input$max=999
-                        input$style='text-align:center;'
-                        input$id="qty"
-                        input$name="qty"
-                        input$tabindex="0"
+                        type="tel"
+                        style="width: 50%; text-align:center;"
+                        step=1
+                        min=0
+                        max=999
+                        id="qty"
+                        name="qty"
+                        placeholder="0"
                         bind:value={selectedRow.qty}
                         on:keypress={handleKeyPress}
                     />
-                    <Textfield
+                    <!-- <Textfield
                         label="Unit"
                         input$style='text-align:center;'
                         style="width: 50%;"
                         input$id="unit"
                         input$name="unit"
                         bind:value={selectedRow.unit}
-                    />
-                    </vertflex>
-                <IconButton touch class="material-icons" on:click={next}>arrow_forward_ios</IconButton>
+                    /> -->
+                </vertflex>
+                <IconButton touch class="material-icons" on:click={next} tabindex="-1">arrow_forward_ios</IconButton>
             </horzflex>
+            <units>
+                {#each ['boxes', 'tubs', 'crates', 'nets', 'sacks'] as unit}
+                    <Button 
+                        color={selectedRow.unit === unit ? "primary" : "secondary"} 
+                        on:click={() => selectedRow.unit = unit}
+                    >
+                        {unit}
+                    </Button>
+                {/each}
+            </units>
+            <hr/>
+            <textarea 
+                id="notes"
+                name="notes"
+                placeholder="Notes"
+                bind:value={selectedRow.notes}
+            />
         </Content>
     </main>
 {/if}
