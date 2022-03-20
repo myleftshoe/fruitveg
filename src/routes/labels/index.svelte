39f2170f1078f0cfcc79c29e80cf82b77b77b211
myleftshoe <paulesammut@gmail.com>
2022-03-20 18:40:16 +1100

    formatted

--- src/routes/labels/index.svelte
+++ src/routes/labels/index.svelte
@@ -2,49 +2,71 @@
     // Inspired by https://svelte.dev/repl/810b0f1e16ac4bbd8af8ba25d5e0deff?version=3.4.2.
     import { flip } from 'svelte/animate'
 
-    let baskets = [
+    const tagGroups = [
         {
-            name: 'Basket 1',
-            items: [
-                'mangoes',
-                'mangoes'
-            ].reverse()
+            name: 'MainFridge',
+            tags: [
+                {
+                    macAddress: 'AD4FC9X4D1',
+                    pluCode: '2000'
+                },
+                {
+                    macAddress: 'AD4FC9X4D2',
+                    pluCode: '2001'
+                },
+                {
+                    macAddress: 'AD4FC9X4D3',
+                    pluCode: '2002'
+                }
+            ]
         },
         {
-            name: 'Basket 2',
-            items: ['Banana', 'Apple']
-        },
-        {
-            name: 'Basket 3',
-            items: ['GrapeFruit']
+            name: 'FruitTop',
+            tags: [
+                {
+                    macAddress: 'AD4FC9X4E1',
+                    pluCode: '3000'
+                },
+                {
+                    macAddress: 'AD4FC9X4E2',
+                    pluCode: '3001'
+                },
+                {
+                    macAddress: 'AD4FC9X4E3',
+                    pluCode: '3001'
+                }
+            ]
         }
     ]
 
     let hoveringOverBasket
 
-    function dragStart(event, basketIndex, itemIndex) {
+    function dragStart(event, groupIndex, tagIndex) {
+        console.log(event)
+
         // The data we want to make available when the element is dropped
         // is the index of the item being dragged and
         // the index of the basket from which it is leaving.
-        const data = { basketIndex, itemIndex }
+        const data = { groupIndex, tagIndex }
         event.dataTransfer.setData('text/plain', JSON.stringify(data))
     }
 
-    function drop(event, basketIndex) {
+    function drop(event, groupIndex) {
         event.preventDefault()
         const json = event.dataTransfer.getData('text/plain')
         const data = JSON.parse(json)
 
         // Remove the item from one basket.
         // Splice returns an array of the deleted elements, just one in this case.
-        const [item] = baskets[data.basketIndex].items.splice(data.itemIndex, 1)
+        const [item] = tagGroups[data.groupIndex].tags.splice(data.tagIndex, 1)
 
         // Add the item to the drop target basket.
-        baskets[basketIndex].items.push(item)
-        baskets = baskets
+        tagGroups[groupIndex].tags.push(item)
+        tagGroups = tagGroups
 
         hoveringOverBasket = null
     }
+    console.log(tagGroups)
 </script>
 
 <style>
@@ -82,25 +104,28 @@
 </style>
 
 <main>
-{#each baskets as basket, basketIndex (basket)}
-    <div animate:flip>
-        <b>{basket.name}</b>
-        <ul
-            class:hovering={hoveringOverBasket === basket.name}
-            on:dragenter={() => (hoveringOverBasket = basket.name)}
-            on:dragleave={() => (hoveringOverBasket = null)}
-            on:drop={event => drop(event, basketIndex)}
-            ondragover="return false">
-            {#each basket.items as item, itemIndex (item)}
-                <div class="item" animate:flip="{{duration: 100}}">
-                    <li
-                        draggable={true}
-                        on:dragstart={event => dragStart(event, basketIndex, itemIndex)}>
-                        {item}
-                    </li>
-                </div>
-            {/each}
-        </ul>
-    </div>
-{/each}
+    {#each tagGroups as group, groupIndex (group.name)}
+        <div animate:flip>
+            <b>{group.name}</b>
+            <ul
+                class:hovering={hoveringOverBasket === group.name}
+                on:dragenter={() => (hoveringOverBasket = group.name)}
+                on:dragleave={() => (hoveringOverBasket = null)}
+                on:drop={event => drop(event, groupIndex)}
+                ondragover="return false"
+            >
+                {#each group.tags as tag, tagIndex (tag.macAddress)}
+                    <div class="item" animate:flip={{ duration: 300 }}>
+                        <li
+                            {tag}
+                            draggable={true}
+                            on:dragstart={event => dragStart(event, groupIndex, tagIndex)}
+                        >
+                            {tag.pluCode}
+                        </li>
+                    </div>
+                {/each}
+            </ul>
+        </div>
+    {/each}
 </main>
