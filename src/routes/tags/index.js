7263ba651248d666d27dad1e9c981f39faaec066
myleftshoe <paulesammut@gmail.com>
2022-03-24 11:30:05 +1100

    products coming from backend

--- src/routes/tags/index.js
+++ src/routes/tags/index.js
@@ -11,7 +11,7 @@ export const get = async ({ params, request }) => {
     console.log('tags.index.js.get')
 
     const response = await minew.get(`/goods?page=1&size=999&storeId=123`)
-    const boundProducts = response.rows.filter(row => ['FRUIT'].includes(row.label13)).map(row => ({
+    const products = response.rows.filter(row => ['FRUIT'].includes(row.label13)).map(row => ({
         id: row.id,
         pluCode: row.label3,
         label4: row.label4,
@@ -27,52 +27,12 @@ export const get = async ({ params, request }) => {
     })).filter(row => row.status === 'bound')
 
     // return {}
-    console.log(boundProducts.length)
+    console.log(products.length)
 
-    // const body = { tags: boundProducts }
+    const body = { products }
 
-    // return { body }
+    return { body }
 
-    const bindings = []
-
-    const limit = RateLimit(5)
-
-    for (const product of boundProducts) {
-        await limit()
-        const response = await getTagBinding(product.id)
-        console.log(product.id)
-        console.log('rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', response)
-        response.forEach((data) => {
-            console.log('fsdfsfds', data.mac)
-            bindings.push({ macAddress: data.mac, id: product.id, pluCode: product.pluCode, description: `${product.label5} ${product.label4}`.trim() })
-        })
-    }
-
-
-    // await Promise.all(boundProducts.slice(35, 39).map(async (row) => {
-    //     const response = await getTagBinding(row.id)
-    //     console.log(row.id)
-    //     console.log('rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr', response)
-    //     // if (!response) return
-    //     // if (!response.length) return
-    //     response.forEach((data) => {
-    //         console.log('fsdfsfds', data.mac)
-    //         bindings.push({ macAddress: data.mac, id: row.id, pluCode: row.pluCode, description: `${row.label5} ${row.label4}`.trim() })
-    //     })
-    //     return response
-    // }))
-    // // preview(ycap)
-    // console.table('bindings', bindings)
-
-    // appendFile('./bindings.json', JSON.stringify(bindings), err => {
-    //     if (err) {
-    //         console.error(err)
-    //         return
-    //     }
-    // })
-
-
-    return { body: { tags: bindings } }
 }
 
 async function preview(macAddress) {
--- src/routes/tags/index.svelte
+++ src/routes/tags/index.svelte
@@ -5,464 +5,6 @@
     import { tagStore } from '$lib/stores/tagStore'
 
 
-    const products = [
-        {
-            "macAddress": "AC233FD0A2EA",
-            "id": "2001",
-            "pluCode": "1638",
-            "description": "BANANAS"
-        },
-        {
-            "macAddress": "AC233FD0A0BA",
-            "id": "2001",
-            "pluCode": "1638",
-            "description": "BANANAS"
-        },
-        {
-            "macAddress": "AC233FD09F50",
-            "id": "2003",
-            "pluCode": "1640",
-            "description": "PINK LADY APPLES"
-        },
-        {
-            "macAddress": "AC233FD09F4D",
-            "id": "2004",
-            "pluCode": "9114",
-            "description": "GALA APPLES"
-        },
-        {
-            "macAddress": "AC233FD0A29F",
-            "id": "2004",
-            "pluCode": "9114",
-            "description": "GALA APPLES"
-        },
-        {
-            "macAddress": "AC233FD0A2EC",
-            "id": "2004",
-            "pluCode": "9114",
-            "description": "GALA APPLES"
-        },
-        {
-            "macAddress": "AC233FD0A3D7",
-            "id": "2002",
-            "pluCode": "1639",
-            "description": "GRANNY SMITH APPLES"
-        },
-        {
-            "macAddress": "AC233FD0A08F",
-            "id": "2002",
-            "pluCode": "1639",
-            "description": "GRANNY SMITH APPLES"
-        },
-        {
-            "macAddress": "AC233FD0A2AC",
-            "id": "2000",
-            "pluCode": "1637",
-            "description": "APPLE BAGS"
-        },
-        {
-            "macAddress": "AC233FD0A474",
-            "id": "2000",
-            "pluCode": "1637",
-            "description": "APPLE BAGS"
-        },
-        {
-            "macAddress": "AC233FD09EFF",
-            "id": "2000",
-            "pluCode": "1637",
-            "description": "APPLE BAGS"
-        },
-        {
-            "macAddress": "AC233FD0A134",
-            "id": "2000",
-            "pluCode": "1637",
-            "description": "APPLE BAGS"
-        },
-        {
-            "macAddress": "AC233FD0A2B6",
-            "id": "2000",
-            "pluCode": "1637",
-            "description": "APPLE BAGS"
-        },
-        {
-            "macAddress": "AC233FD0A2C4",
-            "id": "2006",
-            "pluCode": "9116",
-            "description": "RED SENSATION PEARS"
-        },
-        {
-            "macAddress": "AC233FD09F3E",
-            "id": "2005",
-            "pluCode": "9115",
-            "description": "GOLDEN DEL APPLES"
-        },
-        {
-            "macAddress": "AC233FD0A263",
-            "id": "2005",
-            "pluCode": "9115",
-            "description": "GOLDEN DEL APPLES"
-        },
-        {
-            "macAddress": "AC233FD12649",
-            "id": "2008",
-            "pluCode": "1645",
-            "description": "BANANAS"
-        },
-        {
-            "macAddress": "AC233FD0A48F",
-            "id": "2012",
-            "pluCode": "1649",
-            "description": "BLUEBERRIES PUNNETS"
-        },
-        {
-            "macAddress": "AC233FD0A47D",
-            "id": "2012",
-            "pluCode": "1649",
-            "description": "BLUEBERRIES PUNNETS"
-        },
-        {
-            "macAddress": "AC233FD09B31",
-            "id": "2007",
-            "pluCode": "1644",
-            "description": "HASS AVOCADO"
-        },
-        {
-            "macAddress": "AC233FD0A268",
-            "id": "2007",
-            "pluCode": "1644",
-            "description": "HASS AVOCADO"
-        },
-        {
-            "macAddress": "AC233FD0A36C",
-            "id": "2007",
-            "pluCode": "1644",
-            "description": "HASS AVOCADO"
-        },
-        {
-            "macAddress": "AC233FD09F9D",
-            "id": "2017",
-            "pluCode": "1671",
-            "description": "SMALL AVOCADOS HASS"
-        },
-        {
-            "macAddress": "AC233FD06B45",
-            "id": "2020",
-            "pluCode": "1754",
-            "description": "APRICOTS"
-        },
-        {
-            "macAddress": "AC233FD0A0FF",
-            "id": "2019",
-            "pluCode": "1683",
-            "description": "WHOLE ROCKMELON"
-        },
-        {
-            "macAddress": "AC233FD0A45D",
-            "id": "2013",
-            "pluCode": "1651",
-            "description": "FUJI APPLES"
-        },
-        {
-            "macAddress": "AC233FD0A345",
-            "id": "2013",
-            "pluCode": "1651",
-            "description": "FUJI APPLES"
-        },
-        {
-            "macAddress": "AC233FD069FD",
-            "id": "2025",
-            "pluCode": "1675",
-            "description": "YELLOW NECTARINE"
-        },
-        {
-            "macAddress": "AC233FD0A11C",
-            "id": "2025",
-            "pluCode": "1675",
-            "description": "YELLOW NECTARINE"
-        },
-        {
-            "macAddress": "AC233FD0A313",
-            "id": "2027",
-            "pluCode": "1658",
-            "description": "GREEN GRAPES"
-        },
-        {
-            "macAddress": "AC233FD09F52",
-            "id": "2027",
-            "pluCode": "1658",
-            "description": "GREEN GRAPES"
-        },
-        {
-            "macAddress": "AC233FD0A10F",
-            "id": "2029",
-            "pluCode": "1660",
-            "description": "PEACHES CLINGSTONE"
-        },
-        {
-            "macAddress": "AC233FD09B2B",
-            "id": "2032",
-            "pluCode": "1663",
-            "description": "LEMONS"
-        },
-        {
-            "macAddress": "AC233FD0A33B",
-            "id": "2032",
-            "pluCode": "1663",
-            "description": "LEMONS"
-        },
-        {
-            "macAddress": "AC233FD0692E",
-            "id": "2032",
-            "pluCode": "1663",
-            "description": "LEMONS"
-        },
-        {
-            "macAddress": "AC233FD09B32",
-            "id": "2026",
-            "pluCode": "1657",
-            "description": "HONEYDEW MELON"
-        },
-        {
-            "macAddress": "AC233FD0A071",
-            "id": "2026",
-            "pluCode": "1657",
-            "description": "HONEYDEW MELON"
-        },
-        {
-            "macAddress": "AC233FD0A167",
-            "id": "2026",
-            "pluCode": "1657",
-            "description": "HONEYDEW MELON"
-        },
-        {
-            "macAddress": "AC233FD09F49",
-            "id": "2028",
-            "pluCode": "1659",
-            "description": "CRIMSON GRAPES"
-        },
-        {
-            "macAddress": "AC233FD0A22C",
-            "id": "2028",
-            "pluCode": "1659",
-            "description": "CRIMSON GRAPES"
-        },
-        {
-            "macAddress": "AC233FD0A201",
-            "id": "2028",
-            "pluCode": "1659",
-            "description": "CRIMSON GRAPES"
-        },
-        {
-            "macAddress": "AC233FD0A055",
-            "id": "2037",
-            "pluCode": "1668",
-            "description": "LOOSE ORANGES"
-        },
-        {
-            "macAddress": "AC233FD09B38",
-            "id": "2036",
-            "pluCode": "1667",
-            "description": "WHITE NECTARINES"
-        },
-        {
-            "macAddress": "AC233FD0A30E",
-            "id": "2036",
-            "pluCode": "1667",
-            "description": "WHITE NECTARINES"
-        },
-        {
-            "macAddress": "AC233FD0A3F4",
-            "id": "2033",
-            "pluCode": "1664",
-            "description": "LIMES"
-        },
-        {
-            "macAddress": "AC233FD099A5",
-            "id": "2033",
-            "pluCode": "1664",
-            "description": "LIMES"
-        },
-        {
-            "macAddress": "AC233FD0A2EF",
-            "id": "2038",
-            "pluCode": "1669",
-            "description": "ORANGE NETS"
-        },
-        {
-            "macAddress": "AC233FD09FD7",
-            "id": "2038",
-            "pluCode": "1669",
-            "description": "ORANGE NETS"
-        },
-        {
-            "macAddress": "AC233FD09EE6",
-            "id": "2038",
-            "pluCode": "1669",
-            "description": "ORANGE NETS"
-        },
-        {
-            "macAddress": "AC233FD09D44",
-            "id": "2038",
-            "pluCode": "1669",
-            "description": "ORANGE NETS"
-        },
-        {
-            "macAddress": "AC233FD09D4A",
-            "id": "2038",
-            "pluCode": "1669",
-            "description": "ORANGE NETS"
-        },
-        {
-            "macAddress": "AC233FD0A0AC",
-            "id": "2039",
-            "pluCode": "1684",
-            "description": "MANGOES"
-        },
-        {
-            "macAddress": "AC233FD0A31A",
-            "id": "2039",
-            "pluCode": "1684",
-            "description": "MANGOES"
-        },
-        {
-            "macAddress": "AC233FD0A235",
-            "id": "2039",
-            "pluCode": "1684",
-            "description": "MANGOES"
-        },
-        {
-            "macAddress": "AC233FD0A39B",
-            "id": "2039",
-            "pluCode": "1684",
-            "description": "MANGOES"
-        },
-        {
-            "macAddress": "AC233FD0A4A2",
-            "id": "2050",
-            "pluCode": "1677",
-            "description": "MEDJOOL DATES"
-        },
-        {
-            "macAddress": "AC233FD09FD0",
-            "id": "2040",
-            "pluCode": "7421",
-            "description": "DRAGONFRUIT"
-        },
-        {
-            "macAddress": "AC233FD0A133",
-            "id": "2048",
-            "pluCode": "1675",
-            "description": "PACKHAM PEARS"
-        },
-        {
-            "macAddress": "AC233FD0A2AA",
-            "id": "2047",
-            "pluCode": "1674",
-            "description": "BEURRE BOSC PEARS"
-        },
-        {
-            "macAddress": "AC233FD0A2A2",
-            "id": "2047",
-            "pluCode": "1674",
-            "description": "BEURRE BOSC PEARS"
-        },
-        {
-            "macAddress": "AC233FD0A109",
-            "id": "2047",
-            "pluCode": "1674",
-            "description": "BEURRE BOSC PEARS"
-        },
-        {
-            "macAddress": "AC233FD0A457",
-            "id": "2049",
-            "pluCode": "1676",
-            "description": "NASHI PEARS"
-        },
-        {
-            "macAddress": "AC233FD0A465",
-            "id": "2049",
-            "pluCode": "1676",
-            "description": "NASHI PEARS"
-        },
-        {
-            "macAddress": "AC233FD0A011",
-            "id": "2049",
-            "pluCode": "1676",
-            "description": "NASHI PEARS"
-        },
-        {
-            "macAddress": "AC233FD0A12C",
-            "id": "2055",
-            "pluCode": "1685",
-            "description": "STRAWBERRY PUNNETS"
-        },
-        {
-            "macAddress": "AC233FD0A4C4",
-            "id": "2051",
-            "pluCode": "1678",
-            "description": "PINEAPPLE"
-        },
-        {
-            "macAddress": "AC233FD0A2A3",
-            "id": "2058",
-            "pluCode": "7420",
-            "description": "BLOOD PLUMS"
-        },
-        {
-            "macAddress": "AC233FD0A497",
-            "id": "2059",
-            "pluCode": "8536",
-            "description": "WATERMELON"
-        },
-                {
-            "macAddress": "AC233FD0996F",
-            "id": "2059",
-            "pluCode": "8536",
-            "description": "WATERMELON"
-        },
-        {
-            "macAddress": "AC233FD0A397",
-            "id": "2059",
-            "pluCode": "8536",
-            "description": "WATERMELON"
-        },
-        {
-            "macAddress": "AC233FD09FFE",
-            "id": "2220",
-            "pluCode": "1646",
-            "description": "BANANAS"
-        },
-        {
-            "macAddress": "AC233FD09B8E",
-            "id": "2220",
-            "pluCode": "1646",
-            "description": "BANANAS"
-        },
-        {
-            "macAddress": "AC233FD09FFC",
-            "id": "2220",
-            "pluCode": "1646",
-            "description": "BANANAS"
-        },
-        {
-            "macAddress": "AC233FD09B93",
-            "id": "2220",
-            "pluCode": "1646",
-            "description": "BANANAS"
-        },
-        {
-            "macAddress": "AC233FD09FFD",
-            "id": "2220",
-            "pluCode": "1646",
-            "description": "BANANAS"
-        },
-        {
-            "macAddress": "AC233FD09B8C",
-            "id": "2220",
-            "pluCode": "1646",
-            "description": "BANANAS"
-        }
-    ]
 
     let tagGroups = [
         // {
@@ -583,6 +125,9 @@
         }
     ]
 
+    export let products = []
+
+
     let hoveringOverBasket
     let float, floatRef
 
@@ -592,16 +137,15 @@
         tags = new Map(value)
     })
 
-    function dragStart(e, id) {
-        console.log(id)
-        e.dataTransfer.setData('text/plain', id)
+    function dragStart(e, info) {
+        console.log(info)
+        e.dataTransfer.setData('text/plain', info)
     }
 
     async function remove(e) {
         e.preventDefault()
         float = e.dataTransfer.getData('text/plain')
-        console.log(floatRef)
-
+        console.log(float)
         floatRef.style.top = '100px'
         floatRef.style.backgroundColor = '#00f'
         await tick()
@@ -622,12 +166,12 @@
         console.log(e)
         e.preventDefault()
         e.stopPropagation()
-        const id = e.dataTransfer.getData('text/plain')
+        const [id, descripiton] = e.dataTransfer.getData('text/plain').split(',')
         console.log('dropped', id, toTag)
-        float = toTag.id
+        float = `${toTag.id},${toTag.descripiton}`
         await bind(toTag.macAddress, id)
         // TODO: on success only
-        const product = products.find(product => product.id == id)
+        const product = products.get(toTag.id)
         toTag.id = product.id
         toTag.pluCode = product.pluCode
         toTag.description = product.description
@@ -651,10 +195,19 @@
     li {
         background-color: lightgray;
         cursor: pointer;
-        display: inline-block;
+        display: flex;
+        align-items: center;
+        /* justify-content: center; */
         margin-right: 10px;
-        padding: 10px;
-        /* font-family: WqyBitmapSong_AlibabaPuhui; */
+        padding: 5px;
+        width: calc( 100vw / 20 );
+        height: 80%;
+        font-size: 12px;
+        font-family: 'Roboto Condensed';
+        font-weight: 500;
+        text-transform: lowercase;
+        justify-self: center;
+        overflow: hidden;
     }
     li:hover {
         background: orange;
@@ -663,11 +216,11 @@
     ul {
         border: solid lightgray 1px;
         display: flex; /* required for drag & drop to work when .item display is inline */
-        height: 40px; /* needed when empty */
+        /* height: 40px; needed when empty */
         padding: 10px;
     }
     main {
-        background-color: #0f07;
+        /* background-color: #0f07; */
         height: 100vh;
         touch-action: none;
         font-size: 12px;
@@ -700,7 +253,7 @@
                     <div class="item" animate:flip={{ duration: 300 }}>
                         <li
                             draggable={true}
-                            on:dragstart={e => dragStart(e, tag.id)}
+                            on:dragstart={e => dragStart(e, `${tag.id},${tag.description || 'loading...'}`)}
                             on:drop={e => drop(e, tag)}
                         >
                             {tags.get(tag.macAddress)?.description}
@@ -711,12 +264,11 @@
         </div>
     {/each}
 </main>
-<float
+<float {float}
     bind:this={floatRef}
     style={float && 'display: block;'}
-    {float}
     draggable={true}
     on:dragstart={e => dragStart(e, float)}
 >
-    {float}
+    {float?.split(',')[1]}
 </float>
