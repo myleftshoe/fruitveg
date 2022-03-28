fc9caa9dac844c37abddbb2f04cbf2dce6d91d32
myleftshoe <paulesammut@gmail.com>
2022-03-28 15:13:07 +1100

    .

--- src/routes/tags/index.svelte
+++ src/routes/tags/index.svelte
@@ -9,61 +9,66 @@
 
     import ProductDrawer from '$lib/productDrawer.svelte'
     
+    /* not working tags
+
+    AC233FD0A100 
+
+    */
+
+
     let tagGroups = [
         {
             name: 'Main Fridge', // 19
             tags: [
                 { macAddress: 'AC233FD0A38F', id: '1723' }, // lebq
                 { macAddress: 'AC233FD0A290', id: '1692' }, // green beans
-                { macAddress: 'AC233FD0A100', id: '1695' }, // butter beans ///// TAG NOT WORKIKNG
+                { macAddress: 'AC233FD0A27B', id: '1695' }, // butter beans
                 { macAddress: 'AC233FD0A39C', id: '1697' }, // beet
                 { macAddress: 'AC233FD0A3A4', id: '1697' }, // beet
-                { macAddress: 'AC233FD0A39D', id: '1772' }, // cauii
                 { macAddress: 'AC233FD0A489', id: '1772' }, // cauii
+                { macAddress: 'AC233FD0A39D', id: '1772' }, // cauii
                 { macAddress: 'AC233FD09FB5', id: '1688' }, // baby broc
                 { macAddress: 'AC233FD09FBE', id: '1698' }, // broc
                 { macAddress: 'AC233FD09FBA', id: '1698' }, // broc
                 { macAddress: 'AC233FD0A034', id: '1698' }, // broc
                 { macAddress: 'AC233FD0A1CF', id: '1711' }, // carrrots
-                { macAddress: 'AC233FD0A142', id: '1711' }, // carrrots
                 { macAddress: 'AC233FD09F07', id: '1711' }, // carrrots
+                { macAddress: 'AC233FD0A142', id: '1711' }, // carrrots
                 { macAddress: 'AC233FD09F52', id: '1658' }, // green grapes
                 { macAddress: 'AC233FD0A22C', id: '1659' }, // crimson grapes
                 { macAddress: 'AC233FD0A201', id: '1659' }, // crimson grapes
                 { macAddress: 'AC233FD0A235', id: '1684' }, // mangoes
-                { macAddress: 'AC233FD0A0AC', id: '1684' } // mangoes
+                { macAddress: 'AC233FD0A39B', id: '1684' } // mangoes
             ]
         },
         {
             name: 'Fruit Top', // 10        
-
             tags: [
                 { macAddress: 'AC233FD0A0FF', id: '2019' }, // Rock
+                { macAddress: 'AC233FD0A2AC', id: '2000' }, // AB
                 { macAddress: 'AC233FD0A134', id: '2000' }, // AB
                 { macAddress: 'AC233FD0A2B6', id: '2000' }, // AB
-                { macAddress: 'AC233FD0A2AC', id: '2000' }, // AB
-                { macAddress: 'AC233FD09D4A', id: '1669' }, // ON
                 { macAddress: 'AC233FD09D44', id: '1669' }, // ON 
-                { macAddress: 'AC233FD09F97', id: '1779' }, // Basil
+                { macAddress: 'AC233FD09D4A', id: '1669' }, // ON
                 { macAddress: 'AC233FD0A202', id: '1779' }, // Basil
-                { macAddress: 'AC233FD09D46', id: '1775' }, // Cherry Tom
+                { macAddress: 'AC233FD09F97', id: '1779' }, // Basil
                 { macAddress: 'AC233FD09D4C', id: '1775' }, // Cherry Tom
+                { macAddress: 'AC233FD09D46', id: '1775' }, // Cherry Tom
             ]
         },
         {
             name: 'Fruit Middle', // 10 
-
             tags: [
                 { macAddress: 'AC233FD0A263', id: '9115' }, // Gold Del
                 { macAddress: 'AC233FD0A08F', id: '1639' }, // Granny
                 { macAddress: 'AC233FD0A133', id: '1675' }, // Packham
-                { macAddress: 'AC233FD0A2A2', or: 'AC233FD0A109', id: '1674' }, // Beurre Bosc 
+                { macAddress: 'AC233FD0A109', id: '1675' }, // Packham
                 { macAddress: 'AC233FD0A30E', id: '1667' }, // White Nect
-                { macAddress: '????????????', id: '7420' }, // plum TODO
                 { macAddress: 'AC233FD0A11C', id: '7420' }, // plum
+                { macAddress: '????????????', id: '1674' },
                 { macAddress: 'AC233FD0A132', id: '????' }, // Ruby Grapefruit
-                { macAddress: 'AC233FD0A2B1', id: '1773' }, // round
                 { macAddress: 'AC233FD09A82', id: '1773' }, // round
+                { macAddress: 'AC233FD0A2B1', id: '1773' }, // round
             ]
         },
         {
