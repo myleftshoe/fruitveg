e15ad5617fe60e42045ef9b68c953b12a44fa362
myleftshoe <paulesammut@gmail.com>
2022-03-29 19:33:29 +1100

    .

---
+++ src/routes/textarea.svelte
@@ -0,0 +1,21 @@
+<script>
+
+    let nameRef
+
+    function handleQtyClick(e) {
+        if (e.key === " ") nameRef.focus()
+    }
+
+</script>
+<input id="qty" type="number" min="1" max="99" on:keypress={handleQtyClick}/>
+<input bind:this={nameRef} id="name" type="text"/>
+<style>
+    input {
+        border: none;
+        background-color: #f001
+    }
+    #qty {
+        /* width: 3ch; */
+
+    }
+</style>
