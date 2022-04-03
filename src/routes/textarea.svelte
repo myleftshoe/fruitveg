b146467f246e7888f93f7fecb1c46b164981575f
myleftshoe <paulesammut@gmail.com>
2022-04-03 19:43:35 +1000

    big changes

--- src/routes/textarea.svelte
+++ src/routes/textarea.svelte
@@ -15,39 +15,24 @@
     ]
 </script>
 <script>
-    import { tick } from 'svelte'
     import { browser } from '$app/env';
-
+    import { tick } from 'svelte'
     import { slide as transition } from 'svelte/transition'
-    import List, { Item, Text, PrimaryText, SecondaryText, Meta } from '@smui/list'
     import Dialog, { Header, Title, Content, Actions } from '@smui/dialog'
     import Button from '@smui/button'
     import IconButton from '@smui/icon-button'
-
-    import Autocomplete from '@smui-extra/autocomplete'
-    import Select, { Option } from '@smui/select'
-    import Paper from '@smui/paper'
-    import Textfield from '@smui/textfield'
-    import Icon from '@smui/textfield/icon'
-    import products from '$lib/productStore'
     import clipboard from '../helpers/clipboard.js'
 
-
-    import Drawer from './drawer.svelte'
+    import products from '$lib/productStore'
 
     const refs = {
-        form: null,
         qty: null,
         name: null,
         unit: null,
         hidden: null,
     }
 
-    let fruits = ['Apple', 'Orange', 'Banana', 'Mango']
     let name = ''
-    let unit = ''
-    let qty = ''
-    let textarea = ''
 
     let copied = false
     let warn = false
@@ -62,7 +47,6 @@
         copied = true
         setTimeout(() => copied = false, 1500)
     }
-
 
     function handleQtyClick(e) {
         if (e.key === ' ') {
@@ -101,45 +85,16 @@
                 focused.qty = frac
                 focused.unit = convert[e.key] || focused.unit
                 items = [...items]
-                e.preventDefault()
                 return
             }
             if (e.key === '9') {
-                e.preventDefault()
                 selectedItem = option
                 return
             }
         }
         if (value.length > 1) {
-            e.preventDefault()
             return
         }
-    }
-
-    function handleSelectChange(e) {
-        console.log(e.target.value, name)
-    }
-
-    function handleNameFocus(e) {
-        showItems = name.length > 0
-    }
-
-    let multiple = false
-
-    async function next(e) {
-        console.log('next')
-        textarea = `${textarea}\n${qty} ${name} ${unit}` 
-        refs.form.reset()
-        name = "testset"
-        unit = 'boxes'
-        refs.qty.focus()
-        // refs.qty.select()
-    }
-
-    function handleSubmit(e) {
-        console.log('handle')
-        const fields = refs.form.elements
-        console.table(fields)
     }
 
     const blankOption = {
@@ -161,19 +116,21 @@
     let added = []
 
     function add(e) {
-        e.preventDefault()
         added = [  { ...option }, ...added ]
-        // option = { ...blankOption }
-        // option.name = typed
     }
 
     function remove(e) {
-        e.preventDefault()
         added = added.slice(1)
-        // option = { ...blankOption }
-        // option.name = typed
     }
 
+    let drawerContent = 'products'
+    const setDrawerContent = (name = 'products') => (e) => { drawerContent = name }
+
+    function handleUnitClick(e, unit = '') {
+        option.unit = unit
+    }
+
+    const doNothing = () => {}
 
     $: if (!items.length && $products.length) {   
             items = $products
@@ -197,23 +154,21 @@
 
     $: if (browser && document.activeElement === refs.name) {
         console.log('fsfdssdfsdfsfsdf')
-         name = option.name
+        name = option.name
     }
 
 </script>
 
 <style>
-    main {
+    stocktake {
         align-self: flex-start;
         margin-left: 16px;
     }
     pre {
-        /* flex-grow: 1; */
         font-size: .8em;
     }
-    form {
+    main {
         width: 50vw;
-        /* padding: 16px; */
         display: flex;
         flex-direction: column;
         align-items: center;;
@@ -222,7 +177,7 @@
         height: 100vh;
         gap: 2vh;
     }
-    input, select {
+    input {
         color: #777;
         font-size: 16px;
         border:none;
@@ -247,37 +202,10 @@
     :global(body) {
         margin: 0;
     }
-    float {
-        position: fixed;
-        bottom: 40px;
-        padding: 16px;
-        gap: 32px;
-        right: 40px;
-        background-color: #7773;
-        border-radius: 40px;
-        display:flex;
-        flex-direction: column;
-    }
-    stickybottom {
-        position: fixed;
-        top:0;
-        background-color: #777d;
-        width: 100%;
-        display:flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content:flex-end;
-        height: 17vh;
-    }
     buttons {
-        width:calc( 100% - 8px );
+        width: calc( 100% - 8px );
     }
-
-    select {  
-        padding: 16px;
-        width: 48vw;
-    }
-    products {
+    drawer {
         position: fixed;
         width: 50%;
         top: 0;
@@ -288,7 +216,6 @@
         overflow: scroll;
         overflow-x: hidden;
         background-color: #7773;
-        /* padding: 16px; */
     }
     qty {
         /* background: #7774; */
@@ -306,7 +233,24 @@
         flex-direction:row;
         width: 100%;
         justify-content: space-around;
-        /* align-items: space-between; */
+    }
+    units {
+        display:flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+    more {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: center;
+        height:100%;
+    }
+    section {
+        display:flex;
+        flex-direction: column;
+        gap: 16px;
+        align-items: center;
     }
 
 </style>
@@ -329,8 +273,8 @@
         <Button defaultAction>cancel</Button>
     </Actions>
 </Dialog>
-<form>
-<p></p>
+<main>
+    <IconButton size="button" class="material-icons" style="align-self: flex-start;">clear</IconButton>
     <qty>
         <input
             name="qty"
@@ -345,66 +289,72 @@
         >
         <updown>
             <IconButton class="material-icons" on:click={ (e) => { 
-                e.preventDefault()
                 option.qty++ 
             }}>add</IconButton>
             <IconButton class="material-icons" on:click={ (e) => { 
-                e.preventDefault()
                 option.qty-- 
             }}>remove</IconButton>
         </updown>
     </qty>
-
-    <input name="name" type="text" bind:this={refs.name} bind:value={option.name} on:keydown={(e) => {
-        typed = e.target.value
-        if (e.key.match(/[a-z]/i)) {
+    <input name="name" type="text" bind:this={refs.name} bind:value={option.name} 
+        on:keydown={(e) => {
+            typed = e.target.value
+            if (e.key.match(/[a-z]/i)) {
                 typed = typed + e.key
-        }
-    }}/>
-    <select
-        name="unit"
-        bind:this={refs.unit}
-        bind:value={option.unit}
-        combobox
-        options={units}
-        on:focus={() => {
-            console.log('fff')
-            multiple=true
+            }
         }}
-                    on:blur={() => {
-            console.log('bbb')
-            multiple=false
-        }}
-    >
-        {#each units as unit}
-            <option>{unit}</option>
+        on:focus={setDrawerContent('products')}
+    />
+    <Button on:click={setDrawerContent('units')}>{option.unit === '[none]' ? '[unit]' : option.unit || '[unit]'}</Button>
+    <addremove>
+        <Button class="material-icons" disabled={!added.length} on:click={remove} style="font-weight: bold;">—</Button>
+        <Button class="material-icons" disabled={
+            !option.name || !option.qty || added.find(a => a.name === option.name)
+        } on:click={add}>add</Button>
+    </addremove>
+    <stocktake on:click={() => {refs.name.blur()}}>
+        {#each added as item}
+            <pre>{item.qty} {item.name} {item.unit}</pre>
         {/each}
-    </select>
-<addremove>
-    <Button class="material-icons" disabled={!added.length} on:click={remove} style="font-weight: bold;">—</Button>
-    <Button class="material-icons" disabled={
-        !option.name || !option.qty || added.find(a => a.name === option.name)
-    } on:click={add}>add</Button>
-</addremove>
-<main on:click={() => {refs.name.blur()}}>
-    {#each added as item}
-        <pre>{item.qty} {item.name} {item.unit}</pre>
-    {/each}
+    </stocktake>
+    <drawer>
+        <IconButton size="button" class="material-icons" style="align-self: flex-end;" on:click={setDrawerContent("more")}>more_vert</IconButton>
+        {#if drawerContent === 'units'}
+            <units>
+                {#each units as unit}
+                    <Button style="font-size:10px;" data-value={unit} on:click={(e) => handleUnitClick(e, unit)}>{unit}</Button>
+                {/each}
+            </units>
+        {:else if drawerContent === 'more'}
+            <more>
+                <section>
+                    <Button class="material-icons" size="button" on:click={doNothing}>outside stock</Button>
+                    <Button class="material-icons" size="button" on:click={doNothing}>coolibah</Button>
+                    <Button class="material-icons" size="button" on:click={doNothing}>zuccs, cukes, caps</Button>
+                </section>
+                <section>
+                    <Button class="material-icons" size="button" on:click={() => { 
+                        warn = true 
+                    }}
+                    >copy to clipboard</Button>
+                    <Button class="material-icons" size="button" on:click={() => { 
+                        warn = true 
+                    }}
+                    >start over</Button>
+                </section>
+            </more>
+        {:else}
+            <input name="name" type="text" bind:value={name} style="align-self:center; width: 70%; display: none;"/>
+            {#each options as item}
+                <Button style="justify-content: flex-end" value={item.name} on:click={(e) => {
+                    console.log(e.currentTarget)
+                    option.name = e.currentTarget.value
+                }}><pre>{item.name}</pre></Button>
+            {/each}
+        {/if}
+    </drawer>
 </main>
 
-
-<products>
-<p></p>
-<input name="name" type="text" bind:value={name} style="align-self:center; width: 70%; display: none;"/>
-    {#each options as item}
-        <Button style="justify-content: flex-end" value={item.name} on:click={(e) => {
-            e.preventDefault()
-            console.log(e.currentTarget)
-            option.name = e.currentTarget.value
-        }}><pre>{item.name}</pre></Button>
-    {/each}
-</products>
-</form>
 <buttons>
     <!-- <message style="opacity: {copied ? 1 : 0}">copied!</message> -->
     <IconButton class="material-icons" size="button" on:click={() => { 
