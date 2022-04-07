fc77474d37975d02d1c89adce1a5b9a3e3708c4f
myleftshoe <paulesammut@gmail.com>
2022-04-08 09:01:11 +1000

    .

--- src/routes/fluid/index.svelte
+++ src/routes/fluid/index.svelte
@@ -389,8 +389,8 @@
         position: relative;
         position: sticky;
         position: -webkit-sticky;
-        top: 0vh;
-        /* bottom: 50vh; */
+        /* top: 0vh; */
+        bottom: 90vh;
         margin:-10px;
         box-shadow: 2px 4px 4px #0007;
         padding: 10px 5px 10px 20px; 
