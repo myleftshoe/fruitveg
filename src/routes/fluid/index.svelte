8a83e19c52086b173ac1d33a1d993849b4d53808
myleftshoe <paulesammut@gmail.com>
2022-04-08 21:07:15 +1000

    .

--- src/routes/fluid/index.svelte
+++ src/routes/fluid/index.svelte
@@ -322,11 +322,11 @@
 </main>
 <footer>
 </footer>
-<!-- <fab>
+<fab>
     <Fab on:click={handleFabClick}>
         <Icon class="material-icons">menu</Icon>
     </Fab>
-</fab> -->
+</fab>
 <Dialog bind:open={complete} on:SMUIDialog:closed={null} scrimClickAction={() => complete = false} escapeKeyAction="" >
     <menuDialog>
         <Title>Fruit & Veg Stocktake</Title>
