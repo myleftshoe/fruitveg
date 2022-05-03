110c4f844a6f88a339d45cd8f62cbc76b767e68c
myleftshoe <paulesammut@gmail.com>
2022-05-03 19:26:40 +1000

    .

--- src/routes/camera/index.svelte
+++ src/routes/camera/index.svelte
@@ -2,14 +2,15 @@
 
     let video
     // Prefer camera resolution nearest to 1280x720.
-    const constraints = { audio: false, video: { width: 640, height: 480 } }; 
+    // const constraints = { audio: false, video: { width: 640, height: 480 } }; 
     function connect() {
-        navigator.mediaDevices.getUserMedia(constraints).then(function(mediaStream) {
-            video.srcObject = mediaStream;
-            video.onloadedmetadata = function(e) {
+        navigator.mediaDevices.getUserMedia({video: true}).then(function(stream) {
+            video.srcObject = stream;
+            // video.onloadedmetadata = function(e) {
                 video.play();
-            };
-        }).catch(function(err) { console.log(err.name + ": " + err.message); });
+            // };
+        })
+        // .catch(function(err) { console.log(err.name + ": " + err.message); });
     }
 </script>
 <button on:click={connect}>camera</button>
