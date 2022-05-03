4e1407d3ae3c1dfab4d04d55b4e12405d6f81dd6
myleftshoe <paulesammut@gmail.com>
2022-05-04 09:53:13 +1000

    .

--- src/routes/camera/index.svelte
+++ src/routes/camera/index.svelte
@@ -42,27 +42,27 @@
                 // numOfWorkers: navigator.hardwareConcurrency,
                 constraints,
             },
-            // locator: {
-            //     patchSize: "medium",
-            //     halfSample: true,
-            // },
-            numOfWorkers: 2,
+            locator: {
+                patchSize: "medium",
+                halfSample: false,
+            },
+            numOfWorkers: 1,
             frequency:10,
             decoder: {
-                readers: [{
-                    format: "code_128_reader",
-                    config: {},
-                    // "ean_reader",
-                    // "ean_8_reader",
-                    // "code_39_reader",
-                    // "code_39_vin_reader",
-                    // "codabar_reader",
-                    // "upc_reader",
-                    // "upc_e_reader",
-                    // "i2of5_reader"
-                }],
+                readers: [
+                    "code_128_reader",
+                    "ean_reader",
+                    "ean_8_reader",
+                    "code_39_reader",
+                    "code_39_vin_reader",
+                    "codabar_reader",
+                    "upc_reader",
+                    "upc_e_reader",
+                    "i2of5_reader"
+                ],
             }, 
-            // locate: true,
+            locate: true,
+            src: null,
         }, function (err) {
             if (err) {
                 alert(err);
@@ -79,7 +79,7 @@
         Quagga.onDetected(function (result) {
             // const code = result.codeResult.code
             const code = 'code'
-            alert(`Barcode detected and processed : [${code}]`, result);
+            alert(`Barcode detected and processed : [${code}]`);
             // Quagga.stop()
         });
 
