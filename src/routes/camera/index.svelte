fcbe5bddc5a8414026fe865baf710b6163e78d49
myleftshoe <paulesammut@gmail.com>
2022-05-07 14:15:54 +1000

    .

--- src/routes/camera/index.svelte
+++ src/routes/camera/index.svelte
@@ -25,7 +25,7 @@
                 experimentalFeatures: {
                     useBarCodeDetectorIfSupported: true
                 },
-                aspectRatio: 2.39,
+                aspectRatio: 0.5,
             },
             onScanSuccess,
             onScanFailure,
