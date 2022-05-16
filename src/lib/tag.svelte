61b76585a44b590316efc75e0fb024b9a5e9758d
myleftshoe <paulesammut@gmail.com>
2022-05-16 20:00:10 +1000

    .

--- src/lib/tag.svelte
+++ src/lib/tag.svelte
@@ -17,43 +17,37 @@
 </script>
 <svelte:window bind:innerWidth/>
 {#if product.id}
-    <tag style="height: {height}px; width: {width}px; transform: scale({scale < 1 ? scale : 1});">
-        {#each text as [label, style], i}
-            <span style="
-                top: {style.y}px; 
-                left: {style.x}px; 
-                color: {style.color};
-                font-size: {style['font-size']}px; 
-                font-weight: {style.bold ? 'bold': 'normal'}; 
-                font-family: {style["font-family"]};
-            ">
-                {#if (style['effect'] === 'minitail-up')}
-                    {product[label].split('.')[0]}.
-                    <sup>{product[label].split('.')[1]}</sup>
-                {:else}
-                    {product[label] ?? ''}
-                {/if}
-            </span>
-        {/each}
+<tag style="height: {height}px; width: {width}px; transform: scale({scale < 1 ? scale : 1});">
+    {#each text as [label, style], i}
         <span style="
-            top: {dollar.y}px; 
-            left: {dollar.x}px;
-            font-size: {dollar['font-size']}px; 
-            color: {dollar.color};
-        ">$</span>
-    </tag>
+            top: {style.y}px; 
+            left: {style.x}px; 
+            color: {style.color};
+            font-size: {style['font-size']}px; 
+            font-weight: {style.bold ? 'bold': 'normal'}; 
+            font-family: {style["font-family"]};
+        ">
+            {#if (style['effect'] === 'minitail-up')}
+                {product[label].split('.')[0]}.
+                <sup>{product[label].split('.')[1]}</sup>
+            {:else}
+                {product[label] ?? ''}
+            {/if}
+        </span>
+    {/each}
+    <span style="
+        top: {dollar.y}px; 
+        left: {dollar.x}px;
+        font-size: {dollar['font-size']}px; 
+        color: {dollar.color};
+    ">$</span>
+</tag>
 {/if}
 <style>
-    main {
-        display: grid;
-        place-content: center;
-    }
     tag { 
         display: grid;
-        position: relative;
         background-color: white;
-        /* border: 2px solid black; */
-        box-shadow: 0px 0px 1px 1px black inset;
+        border: 2px solid black;
         transform-origin: center top;
         transition: transform 0.1s ease-out;
     }
